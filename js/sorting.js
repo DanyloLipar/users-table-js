35bb8fd519c63299e9052595562b70a78e382e21
DanyloLipar <danilo.lipar2016@gmail.com>
2022-06-16 11:00:22 +0300

    some update

--- js/sorting.js
+++ js/sorting.js
@@ -1,4 +1,4 @@
-import { allUsers, updater, table } from "./helper";
+import { updater, table } from "./helper";
 
 const ownName = document.getElementById('name');
 const username = document.getElementById('username');
@@ -7,46 +7,46 @@ const userWebsite = document.getElementById('website');
 
 
 ownName.onclick = () => {
-    allUsers.sort((user1, user2) => user1.name.localeCompare(user2.name))
+    const renewed = JSON.parse(localStorage.getItem('users')).sort((user1, user2) => user1.name.localeCompare(user2.name))
     const blocks = document.querySelectorAll('tr.opener');
 
     for (let i = 0; i < blocks.length; i++) {
         blocks[i].remove();
     }
 
-    updater(allUsers, table);
+    updater(renewed, table);
 }
 
 username.onclick = () => {
-    allUsers.sort((user1, user2) => user1.username.localeCompare(user2.username))
+    const renewed = JSON.parse(localStorage.getItem('users')).sort((user1, user2) => user1.username.localeCompare(user2.username))
     const blocks = document.querySelectorAll('tr.opener');
 
     for (let i = 0; i < blocks.length; i++) {
         blocks[i].remove();
     }
 
-    updater(allUsers, table);
+    updater(renewed, table);
 }
 
 userEmail.onclick = () => {
-    allUsers.sort((user1, user2) => user1.email.localeCompare(user2.email))
+    const renewed = JSON.parse(localStorage.getItem('users')).sort((user1, user2) => user1.email.localeCompare(user2.email))
     const blocks = document.querySelectorAll('tr.opener');
 
     for (let i = 0; i < blocks.length; i++) {
         blocks[i].remove();
     }
 
-    updater(allUsers, table);
+    updater(renewed, table);
 }
 
 userWebsite.onclick = () => {
-    allUsers.sort((user1, user2) => user1.website.localeCompare(user2.website))
+    const renewed = JSON.parse(localStorage.getItem('users')).sort((user1, user2) => user1.website.localeCompare(user2.website))
     const blocks = document.querySelectorAll('tr.opener');
 
     for (let i = 0; i < blocks.length; i++) {
         blocks[i].remove();
     }
 
-    updater(allUsers, table);
+    updater(renewed, table);
 }
 
